fa19884f46f597b5f01b01dbe6f732b9bd3371e3
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-15 08:45:17 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,8 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
+def remove_
+  element_from_end_of_array(array)
   array.pop
 end
 
